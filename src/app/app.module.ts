e64ece785fd694f9a59d9ada626cb57c272bcf30
pascaline <passyish5@gmail.com>
2019-08-22 11:30:20 +0200

    added all the necessary things to make an angular app such as components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
