c918040173f98c827e78f6450ecefcdae7eef498
pascaline <passyish5@gmail.com>
2019-08-25 22:21:44 +0200

    modified all files

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,4 @@
-// export class Quote {
-//         id: number;
-//       Author: string;
-//       quote: string;
-//     //   completeDate: Date
-//     }
+
 export class Quote {
     showName: boolean;
     constructor(public id: number,public quote: string,public author: string, public name: string, public completeDate: Date){
--- src/app/votes/votes.component.ts
+++ src/app/votes/votes.component.ts
@@ -6,19 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./votes.component.css']
 })
 export class VotesComponent implements OnInit {
-yesVotes=0
-noVotes=0
-highVotes=5
-upVotes(){
-  this.yesVotes=this.yesVotes+1
-  if(this.yesVotes>this.highVotes){
-    console.log('hhhhh')
-  }
-}
-downVotes(){
-  this.noVotes=this.noVotes+1
-}
-  constructor() { }
+onstructor() { }
 
   ngOnInit() {
   }
